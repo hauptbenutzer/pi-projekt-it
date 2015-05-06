721e43fbd307f3830df77198bc28c70d9b506f5d
Alexander Schramm <alexanderschramm@me.com>
2015-05-06 09:50:13 +0200

    setup maven for photodiode project

---
+++ pi-photoblink/src/main/java/de/uni_luebeck/itm/sva08/App.java
@@ -0,0 +1,10 @@
+package de.uni_luebeck.itm.sva08;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

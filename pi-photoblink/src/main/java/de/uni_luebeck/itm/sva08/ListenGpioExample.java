4cb5cfc21faafa5f5767075403bebd9e5dd87929
Alexander Schramm <alexanderschramm@me.com>
2015-05-12 12:25:32 +0200

    Changed pin numbering and setup maven

--- pi-photoblink/src/main/java/de/uni_luebeck/itm/sva08/ListenGpioExample.java
+++ pi-photoblink/src/main/java/de/uni_luebeck/itm/sva08/ListenGpioExample.java
@@ -4,9 +4,9 @@ package de.uni_luebeck.itm.sva08;
  * **********************************************************************
  * ORGANIZATION  :  Pi4J
  * PROJECT       :  Pi4J :: Java Examples
- * FILENAME      :  ListenGpioExample.java  
- * 
- * This file is part of the Pi4J project. More information about 
+ * FILENAME      :  ListenGpioExample.java
+ *
+ * This file is part of the Pi4J project. More information about
  * this project can be found here:  http://www.pi4j.com/
  * **********************************************************************
  * %%
@@ -16,12 +16,12 @@ package de.uni_luebeck.itm.sva08;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -29,11 +29,11 @@ package de.uni_luebeck.itm.sva08;
  */
 
 import com.pi4j.io.gpio.*;
-import com.pi4j.io.gpio.trigger.GpioSyncStateTrigger;
+import com.pi4j.io.gpio.trigger.*;
 
 /**
  * This example code demonstrates how to setup a listener
- * for GPIO pin state changes on the Raspberry Pi.  
+ * for GPIO pin state changes on the Raspberry Pi.
  *
  * @author Robert Savage
  */
@@ -46,8 +46,8 @@ public class ListenGpioExample {
         final GpioController gpio = GpioFactory.getInstance();
 
         // provision gpio pin #02 as an input pin with its internal pull down resistor enabled
-        final GpioPinDigitalInput sensor = gpio.provisionDigitalInputPin(RaspiPin.GPIO_03);
-        final GpioPinDigitalOutput led = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04);
+        final GpioPinDigitalInput sensor = gpio.provisionDigitalInputPin(RaspiPin.GPIO_00);
+        final GpioPinDigitalOutput led = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07);
 
         sensor.addTrigger(new GpioSyncStateTrigger(led));
 
@@ -59,6 +59,6 @@ public class ListenGpioExample {
 
         // stop all GPIO activity/threads by shutting down the GPIO controller
         // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
-        // gpio.shutdown();   <--- implement this method call if you wish to terminate the Pi4J GPIO controller        
+        // gpio.shutdown();   <--- implement this method call if you wish to terminate the Pi4J GPIO controller
     }
 }
